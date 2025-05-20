243b769ef9ff94d6000736bae08cf2b8bffee2b9
mihir2162 <amovieshome@gmail.com>
2025-05-20 12:20:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "28732126"
+api_hash = "cd699ba42b1a16a0aa0d95e279a5c6d7"
+bot_token = "7476628127:AAEJ_Und5P1NaZXBWXFzdVhj8CXaWe61JQs"
 auth_users = [7844869163]
